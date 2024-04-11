89fb275ccf7c31506da21c4732da6838fb209ad9
Marwa Chaari <marwachaarie@gmail.com>
2024-04-11 18:15:07 +0100

    Three login types

--- src/app/vue/login/login.component.ts
+++ src/app/vue/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { RegistrationService } from 'src/app/controller/registration.service';
-import { User } from 'src/app/model/user';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +9,8 @@ import { User } from 'src/app/model/user';
 })
 export class LoginComponent {
 
-  user = new User();
+  emailId: string;
+  password: string;
   msg = '';
 
   constructor(
@@ -18,15 +18,44 @@ export class LoginComponent {
     private _route: Router) {}
 
   loginUser() {
-    this.registrationService.loginUser(this.user.emailId, this.user.password)
+    this.registrationService.loginUser(this.emailId, this.password)
       .subscribe(
         data => {
           if (data && typeof data === 'object') {
-            //console.log("Response received", data);
-            // const userId = this.registrationService.getUserId(data.token);
-            // const username = this.registrationService.getUserName(data.token);
-            // console.log(userId);
-            // console.log(username);
+            this._route.navigate(['home']);
+          } else {
+            console.error("Invalid JSON response"/*, data*/);
+          }
+        },
+        error => {
+          console.log("Login failed", error);
+          this.msg = "Sign in unsuccessful!\nThe email address and password combination you entered does not match our records. Please try again.";
+        }
+      );
+  }
+
+  loginAdmin() {
+    this.registrationService.loginAdmin(this.emailId, this.password)
+      .subscribe(
+        data => {
+          if (data && typeof data === 'object') {
+            this._route.navigate(['dashboard']);
+          } else {
+            console.error("Invalid JSON response"/*, data*/);
+          }
+        },
+        error => {
+          console.log("Login failed", error);
+          this.msg = "Sign in unsuccessful!\nThe email address and password combination you entered does not match our records. Please try again.";
+        }
+      );
+  }
+
+  loginDeliveryPerson() {
+    this.registrationService.loginDeliveryPerson(this.emailId, this.password)
+      .subscribe(
+        data => {
+          if (data && typeof data === 'object') {
             this._route.navigate(['home']);
           } else {
             console.error("Invalid JSON response"/*, data*/);

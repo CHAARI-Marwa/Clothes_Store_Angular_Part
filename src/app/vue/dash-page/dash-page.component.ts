849ff2563479feb3980055d269aaa3a220a365fb
Trigui Amal <triguiamal12@gmail.com>
2024-04-20 22:03:32 +0100

    dashboard page done dynamically

--- src/app/vue/dash-page/dash-page.component.ts
+++ src/app/vue/dash-page/dash-page.component.ts
@@ -1,8 +1,12 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import Chart from 'chart.js/auto';
+import { CategoryService } from 'src/app/controller/category.service';
+import { CommandService } from 'src/app/controller/command.service';
 import { DeliveryPersonService } from 'src/app/controller/delivery-person.service';
 import { ProductService } from 'src/app/controller/product.service';
 import { RegistrationService } from 'src/app/controller/registration.service';
+import { Category } from 'src/app/model/category';
+import { Command } from 'src/app/model/command';
 import { Delivery_person } from 'src/app/model/deliveryPerson';
 import { Product } from 'src/app/model/product';
 import { User } from 'src/app/model/user';
@@ -13,13 +17,16 @@ import { User } from 'src/app/model/user';
   styleUrls: ['./dash-page.component.css']
 })
 export class DashPageComponent implements AfterViewInit, OnInit{
+
   products: any[] = [];
   delivery: any[]= [];
   users: User[]=[];
   productsCount: number = 0;
   deliverycount: number=0 ;
   usercount:number =0 ;
-  constructor(private productService: ProductService , private deliveryperson : DeliveryPersonService , private registrationservice : RegistrationService) { }
+  constructor(private productService: ProductService , private deliveryperson : DeliveryPersonService , private registrationservice : RegistrationService,
+     private commandeservice : CommandService , private categoryService : CategoryService
+  ) { }
   ngOnInit(): void {
     this.getProducts();
     this.getdelivery();
@@ -68,60 +75,137 @@ this.deliveryperson.getdeliverypersons().subscribe(
   }
   
  
-  customers: number = 1500;
-  ordersPerMonth = {
-    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-    data: [120, 150, 200, 180, 250, 300, 280, 320, 270, 220, 190, 230]
-  };
-  ordersDeliveredPerDay = {
-    labels: ['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6', 'Day 7'],
-    data: [30, 40, 50, 45, 55, 60, 55]
-  };
-  deliveryAgents: number = 20;
+ 
 
-  @ViewChild('ordersPerMonthChart') ordersPerMonthChart: ElementRef;
+  async getAllCommands(): Promise<Command[]> {
+    try {
+     
+      const commands = await this.commandeservice.getAllCommands().toPromise();
+      return commands || []; 
+    } catch (error) {
+      console.error('Erreur lors de la récupération des commandes :', error);
+      return [];
+    }
+  }
+
+  async calculateOrdersPerDay(): Promise<{ [date: string]: number }> {
+    const commands = await this.getAllCommands();
+    const ordersPerDay: { [date: string]: number } = {};
+
+    
+    commands.forEach(command => {
+     
+      const date = new Date(command.date).toISOString().split('T')[0]; 
+
+    
+      ordersPerDay[date] = ordersPerDay[date] ? ordersPerDay[date] + 1 : 1;
+    });
+
+    return ordersPerDay;
+  }
+
+  @ViewChild('ordersPerDayChart') ordersPerDayChart: ElementRef;
   @ViewChild('columnChart') columnChart: ElementRef;
 
   ngAfterViewInit() {
-    this.drawOrdersPerMonthChart();
+    this.drawOrdersPerDayChart();
     this.drawColumnChart();
   }
 
-  private drawOrdersPerMonthChart() {
-    const ctx = this.ordersPerMonthChart.nativeElement.getContext('2d');
+  private async drawOrdersPerDayChart() {
+    const ordersPerDayData = await this.calculateOrdersPerDay(); 
+    const dates = Object.keys(ordersPerDayData);
+    const ordersPerDay = Object.values(ordersPerDayData);
+  
+    const ctx = this.ordersPerDayChart.nativeElement.getContext('2d'); 
     new Chart(ctx, {
       type: 'line',
       data: {
-        labels: this.ordersPerMonth.labels,
+        labels: dates, 
         datasets: [{
-          label: 'Orders per Month',
-          data: this.ordersPerMonth.data,
+          label: 'Orders per Day',
+          data: ordersPerDay,
           borderColor: '#e5c3d1',
           backgroundColor: '#e5c3d1',
           fill: false
+         
         }]
       },
       options: {
         scales: {
           y: {
-            beginAtZero: true
-          }
+            beginAtZero: true,
+            ticks: {
+              stepSize: 1 ,
+              padding: 3
+            }
+          },
+          x: {
+            ticks: {
+              autoSkip: true, 
+              maxTicksLimit: 10, 
+              maxRotation: 0 ,
+              padding: 5
+            }
+          },
         }
+      
       }
     });
   }
 
-  private drawColumnChart() {
+
+  async getTotalByCategory(): Promise<{ [categoryId: string]: number }> {
+    const commands = await this.getAllCommands();
+    const totalByCategory: { [categoryId: string]: number } = {};
+
+
+    for (const command of commands) {
+  
+      for (const commandLine of command.command_lines) {
+        const categoryId = commandLine.product.fcategory_id;
+
+        
+        totalByCategory[categoryId] = (totalByCategory[categoryId] || 0) + commandLine.quantity;
+      }
+    }
+
+    return totalByCategory;
+  }
+
+  async getCategoryNameById(categoryId: number): Promise<string> {
+    try {
+      const category = await this.categoryService.getCategoryById(categoryId).toPromise();
+      if (category) {
+        return category.name || 'Unknown';
+      } else {
+        console.error(`Aucune catégorie trouvée avec l'ID ${categoryId}`);
+        return 'Unknown';
+      }
+    } catch (error) {
+      console.error(`Erreur lors de la récupération de la catégorie avec l'ID ${categoryId} :`, error);
+      return 'Unknown';
+    }
+  }
+  
+  private async drawColumnChart() {
+    const totalByCategory = await this.getTotalByCategory();
+    const categories = Object.keys(totalByCategory);
+    const counts = Object.values(totalByCategory);
+
+    
+    const categoryNames = await Promise.all(categories.map(categoryId => this.getCategoryNameById(+categoryId)));
+
     const ctx = this.columnChart.nativeElement.getContext('2d');
     new Chart(ctx, {
       type: 'bar',
       data: {
-        labels: ['Catégorie 1', 'Catégorie 2', 'Catégorie 3'],
+        labels: categoryNames, 
         datasets: [{
-          label: 'Ventes',
-          data: [50, 60, 70], 
-          backgroundColor: '#B97989', 
-          borderColor: '#B97989', 
+          label: 'Sales by category',
+          data: counts,
+          backgroundColor: '#B97989',
+          borderColor: '#B97989',
           borderWidth: 1,
           barPercentage: 0.5
         }]
@@ -131,7 +215,7 @@ this.deliveryperson.getdeliverypersons().subscribe(
           y: {
             beginAtZero: true,
             ticks: {
-              stepSize: 5 // Ajustez la taille des pas sur l'axe y (diminuez l'échelle)
+              stepSize: 1
             }
           }
         }
